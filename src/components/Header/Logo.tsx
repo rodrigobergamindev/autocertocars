0fc65d01c9dfce6434c90916ecaef71973e82c6a
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-10-05 16:34:02 -0300

    .

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,16 +1,15 @@
 
-import {Text} from '@chakra-ui/react'
+import { Image, Stack } from "@chakra-ui/react"
 
 export default function Logo () {
     return (
-        <Text
-        fontSize="3xl"
-        fontWeight="bold"
-        letterSpacing="tight"
-        w="64"
-        >
-            autocerto
-            <Text fontWeight="bold" color="yellow.400" ml="1" as="span">.</Text>
-            </Text>
+        <Stack direction="row">
+            <Image
+                boxSize="250px"
+                objectFit="contain"
+                src="/img/logo.png"
+                alt="logo"
+            />
+        </Stack>
     )
 }
