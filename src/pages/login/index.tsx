acf7fe58b5c9cae67a6c53803f3eea2eb2643614
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-10-06 11:32:06 -0300

    .

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,13 @@
 import { Flex, Button, Stack, Box, Text, Avatar} from '@chakra-ui/react'
 import Logo from '../../components/Header/Logo'
-
 import { useSession, signIn, signOut } from 'next-auth/client'
+import { useRouter } from "next/router";
 
 export default function SignIn() {
 
   const [session] = useSession()
-
+  const {asPath} = useRouter()
+  const URL_ERROR = '/login?error=AccessDenied#'
 
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center" direction="column">
@@ -19,13 +20,12 @@ export default function SignIn() {
         flexDirection="column"
       >
         
-        
+        {asPath === URL_ERROR && (
+          <Text fontSize="sm" align="center" mb="4">Acesso negado, credenciais incorretas</Text>
+        )}
        <Button colorScheme="yellow" size="lg" onClick={(): Promise<void> => signIn('google', { callbackUrl: 'http://localhost:3000/dashboard/' })}> Login </Button>
        {!!session && (
          <Box flex="1" p="6">  
-        <Avatar size="2xl" name={session.user.name} src={session.user.image}/>
-           <Text fontWeight="normal">{session.user.email}</Text>
-           <Text fontWeight="normal">{session.user.name}</Text>
           <Button colorScheme="yellow" size="lg" onClick={() => signOut({ callbackUrl: 'http://localhost:3000/' })}> Sair </Button>
          </Box>
     )}
