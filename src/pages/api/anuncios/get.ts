9e5510e535e2c916928c270e6ce0acbb0b5242a3
rodrigobergamindev <rodrigobergamindev@gmail.com>
2021-10-12 23:37:19 -0300

    .

--- src/pages/api/anuncios/get.ts
+++ src/pages/api/anuncios/get.ts
@@ -16,14 +16,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     
     if(req.method !== "GET") {
-        return res.status(405).json({ message: 'não ta vindo delete'})
+        return res.status(405).json({ message: 'não ta vindo get'})
     }
 
     if(session) {
     
+        
 
-        return await prisma.anuncio.findMany()
-
+        const anunciosToShow = await prisma.anuncio.findMany()
+        console.log(anunciosToShow)
     }
     
     res.json({message: "Ok"})
--- src/pages/dashboard/anuncios/index.tsx
+++ src/pages/dashboard/anuncios/index.tsx
@@ -66,7 +66,7 @@ export default function AnuncioList({initialValues, session}) {
             const newAnuncios = await fetch('/api/anuncios/get', {
                 method: "GET"
             })
-            console.log(newAnuncios)
+            console.log(newAnuncios.json())
         
     }
 
