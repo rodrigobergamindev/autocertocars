24ebacef292d70e080646452d1dfc6cbadf1ecb1
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-11-10 14:54:02 -0300

    .

--- src/components/Section/Veiculos/index.tsx
+++ src/components/Section/Veiculos/index.tsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image'
 import "swiper/css";
 import Link from 'next/link'
-import {useState} from 'react'
+
 
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
@@ -48,8 +48,31 @@ export default function VehicleSection({anuncios}: AnuncioProps) {
 
 
     return (
-        <Stack>{anuncios.map(anuncio => (
-            <Text>{anuncio.name}</Text>
-        ))}</Stack>
+        <Stack 
+           spacing={10} 
+            align="center" 
+            justify="center"
+            w="100%"
+            h="100vh"
+            flexDirection="column"
+            backgroundColor="gray.50"
+            
+            >
+            
+                <Box width="100%" maxW="1480px" p={6}  as={Flex}  align="center" flexDirection="row" alignSelf="center">
+
+                
+                <Heading fontWeight="bold" letterSpacing={2} mr={15} fontSize="3xl"  color="gray.800" zIndex={2}>
+                    VEÍCULOS
+                </Heading>
+                <Divider bg="yellow.400" h="3px" width="100%"/>
+               
+
+                </Box>
+               <Flex p={6}  height="550px" maxWidth="1480px" width="100%">
+
+               {anuncios.map(anuncio => <Text>{anuncio.name}</Text>)}
+               </Flex>
+            </Stack>
     )
 }
