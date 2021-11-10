ed997b9a8fe983d68e0b6274b29f7b00cb0dc900
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-11-10 13:37:42 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,12 @@ import Feed from '../components/Section/Feed/index'
 import Vender from '../components/Section/Vender/index'
 import {prisma} from '../../db'
 import Head from 'next/head'
-
+import {useState} from 'react'
 
 
 export default function Home({anuncios, feed}) {
 
-  
+  const [anunciosToShow, setAnuncios] = useState(anuncios)
 
     
     return (
@@ -25,8 +25,8 @@ export default function Home({anuncios, feed}) {
            <Head>
             <title>AutoCerto Cars - O veículo certo para você!</title>
             </Head>
-            <Header anuncios={anuncios}/>
-            <VehicleSection anuncios={anuncios}/>
+            <Header anuncios={anunciosToShow}/>
+            <VehicleSection anuncios={anunciosToShow}/>
             <About/>
             <Feed feed={feed}/>
             <Vender/>
