ff962a3d9b59fc3a21a46059ae107f58e7a5c6c3
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-11-22 16:50:46 -0300

    .

--- src/pages/anuncios/[slug].tsx
+++ src/pages/anuncios/[slug].tsx
@@ -92,7 +92,6 @@ export default function Anuncio({anuncio, anuncios}) {
                              width="100%"
                              height="100%"
                              priority
-                            quality={100}
                              
                              transition="all 0.2s ease-in-out"
                              _hover={{
