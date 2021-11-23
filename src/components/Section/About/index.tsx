0ab2d15e3eed2669e56ac76bff26c78f3ea5e836
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-11-23 13:49:05 -0300

    .

--- src/components/Section/About/index.tsx
+++ src/components/Section/About/index.tsx
@@ -44,7 +44,7 @@ export default function About() {
 
                <Grid gap={[0,0]} w="100%"
             maxW="1400px" templateColumns={["repeat(1,1fr)","repeat(2, 1fr)"]}>
-                   <Stack direction="column" w="100%" h="100%" spacing="16"  p={[0,10]}>
+                   <Stack direction="column" w="100%" h="100%" spacing="16"  p={[0,10]} pt={["5rem",0]}>
 
                    <Heading fontWeight="bold" color="yellow.400" fontSize={["2xl","3xl"]} letterSpacing={8}>VALORES</Heading>
                     <Stack w="100%" direction="column" align="flex-start" >
